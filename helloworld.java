7ea8745191358db426670b4fbbe3118055c74ef4
Karthik Shetty <107351931+KarthikShetty5@users.noreply.github.com>
2023-08-31 13:43:33 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,9 +1,9 @@
 
-class HelloWorld {
+public class helloworld {
 	// Your program begins with a call to main().
 	// Prints "Hello, World" to the terminal window.
 	public static void main(String args[])
 	{
-		System.out.println("Hello, World");
+		System.out.println("Hello, World karthik shetty");
 	}
 }
